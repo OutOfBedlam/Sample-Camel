d2c63d144f64fb8189fd440e173f32f4fd365528
Kwon, Yeong-Eon <happylogin@gmail.com>
2019-03-10 14:39:52 +0900

    database polling

--- build.sbt
+++ build.sbt
@@ -5,9 +5,12 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 libraryDependencies ++= Seq(
-  "org.apache.camel" % "camel-core"
+  "org.apache.camel" % "camel-core",
+  "org.apache.camel" %"camel-sql"
 ).map( _ % "2.21.5")
 
 libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % "1.0.13"
 )
+
+libraryDependencies += "com.h2database" % "h2" % "1.4.198"
---
+++ src/main/scala/com/samples/camel/ex003/Ex003Main.scala
@@ -0,0 +1,74 @@
+package com.samples.camel.ex003
+
+import java.io.File
+
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.impl.{DefaultCamelContext, SimpleRegistry}
+import org.h2.jdbcx.JdbcDataSource
+
+object Ex003Main extends App {
+
+  val basedir = "/tmp/camel_example/data"
+
+  // delete existing db file so that new one can be created
+  Seq("h2db.mv.db", "h2db.trace.db").foreach( n => new File(basedir, n).delete() )
+
+  // 🅐 h2 database setup
+  val ds = new JdbcDataSource
+  ds.setURL(s"jdbc:h2:$basedir/h2db")
+  ds.setUser("sa")
+  ds.setPassword("sa")
+
+  // create table and insert test data
+  val conn = ds.getConnection
+  val stmt1 = conn.createStatement()
+  stmt1.executeUpdate(
+    """
+      |create table test_msg(
+      |   ID INT PRIMARY KEY,
+      |   SUBJECT VARCHAR(80),
+      |   BODY VARCHAR(1000),
+      |   EMAIL VARCHAR(40),
+      |   STATUS CHAR(1)
+      |)
+    """.stripMargin)
+
+  val data = Seq(
+    (1, "Mail for Smith", "Dear Smith,\n This is a notification email",
+      "smith@email.com", "R"),
+    (2, "Mail for Marry", "Dear Marry,\n This is a notification email",
+      "marry@email.com", "R"),
+    (3, "Mail for John", "Dear John,\n This is a notification email",
+      "john@email.com", "R"),
+  )
+  data.foreach{ r =>
+    val stmt = conn.prepareStatement("insert into test_msg values(?, ?, ?, ?, ?)")
+    stmt.setInt(1, r._1)
+    stmt.setString(2, r._2)
+    stmt.setString(3, r._3)
+    stmt.setString(4, r._4)
+    stmt.setString(5, r._5)
+    stmt.executeUpdate()
+  }
+  conn.close()
+
+  // 🅑 register DataSource so that Camel route can find/use database
+  val registry = new SimpleRegistry
+  registry.put("ds", ds)
+
+  // 🅒 camel context & routes
+  val context = new DefaultCamelContext(registry)
+  context.addRoutes(new RouteBuilder() {
+    override def configure(): Unit = {
+      val sqlSelect = "select * from test_msg where status = 'R'"
+      val markSuccess = "update test_msg set status = 'S' where id = :#id"
+      val markFailure = "update test_msg set status = 'F' where id = :#id"
+      from(s"sql:$sqlSelect?dataSource=ds&onConsume=$markSuccess&onConsumeFailed=$markFailure")
+        .to("log:samples.Ex003Main")
+    }
+  })
+
+  context.start()
+  Thread.sleep(10000)
+  context.stop()
+}
